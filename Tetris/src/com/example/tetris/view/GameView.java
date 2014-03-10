bbbde36678a5812d5e7d9831f7275ada77270dfa
handsomezhou <quanjunzhou@gmail.com>
2014-03-10 21:42:05 +0800

    update

--- Tetris/src/com/example/tetris/view/GameView.java
+++ Tetris/src/com/example/tetris/view/GameView.java
@@ -1,7 +1,12 @@
 package com.example.tetris.view;
 
+import com.example.tetris.R;
+import com.example.tetris.board.GameService;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.GestureDetector.OnGestureListener;
@@ -13,12 +18,33 @@ import android.widget.Toast;
 public class GameView extends View implements OnGestureListener,
 		OnTouchListener {
 	GestureDetector mGestureDetector;
+	// 游戏逻辑的实现类
+	private GameService gameService;
+
+	// 俄罗斯方块图片
+	// private Bitmap block_color[];
 
 	public GameView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		mGestureDetector = new GestureDetector(context, this);
 
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		float distance = e2.getX() - e1.getX();
+		if (distance > 0) {
+			Toast.makeText(getContext(), "I'm onFling!---right",
+					Toast.LENGTH_SHORT).show();
+		} else {
+			Toast.makeText(getContext(), "I'm onFling!---left",
+					Toast.LENGTH_SHORT).show();
+		}
+
+		return true;
 	}
 
 	@Override
@@ -31,33 +57,15 @@ public class GameView extends View implements OnGestureListener,
 		// .setText("I'm onDown");
 
 		System.out.println("---------------------");
-		Toast.makeText(getContext(), "I'm onDown!",
-				Toast.LENGTH_SHORT).show();
-		return true;
-	}
-
-	@Override
-	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-			float velocityY) {
-		// TODO Auto-generated method stub
-		float distance=e2.getX()-e1.getX();
-		if(distance>0){
-			Toast.makeText(getContext(), "I'm onFling!---right",
-					Toast.LENGTH_SHORT).show();
-		}else{
-			Toast.makeText(getContext(), "I'm onFling!---left",
-					Toast.LENGTH_SHORT).show();
-		}
-
-		
+		Toast.makeText(getContext(), "I'm onDown!", Toast.LENGTH_SHORT).show();
 		return true;
 	}
 
 	@Override
 	public void onLongPress(MotionEvent e) {
 		// TODO Auto-generated method stub
-		Toast.makeText(getContext(), "I'm onLongPress!",
-				Toast.LENGTH_SHORT).show();
+		Toast.makeText(getContext(), "I'm onLongPress!", Toast.LENGTH_SHORT)
+				.show();
 	}
 
 	@Override
@@ -90,6 +98,47 @@ public class GameView extends View implements OnGestureListener,
 		// TODO Auto-generated method stub
 
 		mGestureDetector.onTouchEvent(event);
+
 		return true;
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		// super.onDraw(canvas);
+
+		Block[][] block = gameService.getBlocks();
+
+		Bitmap bm = BitmapFactory.decodeResource(this.getResources(),
+				R.drawable.block_blue);
+		// canvas.drawBitmap(bm, 100, 100, null);
+		int y = 0;
+		int x = 0;
+		if (null != block) {
+			// canvas.drawBitmap(bm, 200, 100, null);
+			for (int i = 0; i < block.length; i++) {
+				for (int j = 0; j < block[i].length; j++) {
+					canvas.drawBitmap(bm, 0 + block[i][j].getIndexX() * 40,
+							0 + block[i][j].getIndexY() * 40, null);
+
+					// canvas.drawBitmap(bm, block[i][j].getIndexX(),
+					// block[i][j].getIndexY(), null);
+					// canvas.drawBitmap(bm, x+=40, y+=40, null);
+					//
+					// canvas.drawBitmap(BitmapFactory.decodeResource(
+					// this.getResources(), j),
+					// R.drawable.block_blue),0+block[i][j].getIndexX()*40,0+block[i][j].getIndexY()+40
+					// , null);
+				}
+
+			}
+		}
+		// System.out.printf("on Draw++++++++++++++++++++++++++++ block=%p",block);
+		// canvas.drawBitmap(bm, 300, 100, null);
+		System.out.printf("on Draw++++++++++++++++++++++++++++");
+	}
+
+	public void setGameService(GameService gameService) {
+		this.gameService = gameService;
 	}
 }
